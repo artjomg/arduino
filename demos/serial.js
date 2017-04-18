ee98d3f14b89a2cf62177fad5ff3990cd448882c
Artjom Galliardt <galliardt@e-inductive.com>
2017-04-19 00:34:26 +0200

    TST: usb connect

--- demos/serial.js
+++ demos/serial.js
@@ -12,11 +12,11 @@ var serial = {};
   serial.requestPort = function() {
     const filters = [
       /*{ 'vendorId': 0x2341, 'productId': 0x8036 },
-      { 'vendorId': 0x2341, 'productId': 0x8037 },*/
-      { 'vendorId': 0x04d8 , 'productId': 0x00df },
-      { 'vendorId': 0x00df  , 'productId': 0x04d8 },
+      { 'vendorId': 0x2341, 'productId': 0x8037 },
+      { 'vendorId': 0x04d8 , 'productId': 0x00df },*/
+      { 'vendorId': 0x04d8 },
     ];
-    return navigator.usb.requestDevice("/dev/cu.usbmodem1421").then(
+    return navigator.usb.requestDevice({ 'filters': filters }).then(
       device => new serial.Port(device)
     );
   }
