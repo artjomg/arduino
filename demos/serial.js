da712f6bf92c1c40c90cdeb093368d5161be9199
Artjom Galliardt <galliardt@e-inductive.com>
2017-04-19 00:56:50 +0200

    TST

--- demos/serial.js
+++ demos/serial.js
@@ -36,7 +36,7 @@ var serial = {};
     };
 
     return this.device_.open()
-    .then(() => device.selectConfiguration(1))
+    .then(() => device.selectConfiguration(0))
     .then(() => device.claimInterface(device.configuration.interfaces[0].interfaceNumber));
         /*.then(() => {
           if (this.device_.configuration === null) {
